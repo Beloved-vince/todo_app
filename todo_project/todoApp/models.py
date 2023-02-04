7a56bb13be31f4e656d7bedf7e8ca04e6cfaed69
Beloved-vince <vinceoludare@gmail.com>
2023-02-04 15:46:12 +0100

    models update

--- todo_project/todoApp/models.py
+++ todo_project/todoApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+class User(forms.ModelForm):
+    first_name = forms.CharField(max_length=200)
+    last_name = forms.CharField(max_length=200)
+    phone_number = forms.IntegerField()
+
+class TodoList(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    details = models.TextField(max_length=500)
+    date = models.DateTimeField(default=timezone.now)
